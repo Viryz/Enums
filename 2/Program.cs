966fb4adb0d7b1b1c9b062ad852cf08b798dcbf3
Viryz <viryz1998@gmail.com>
2018-06-09 21:10:42 +0300

    Добавьте файлы проекта.

---
+++ 2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    enum Posts
+    {
+        Post1 = 100,
+        Post2 = 300,
+        Post3 = 500
+    }
+
+    static class Accauntant
+    {
+        static public bool AskForBonus(Posts worker, int hours)
+        {
+            if (hours > (int)worker)
+                return true;
+            else return false;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posts worker = Posts.Post2;
+            if (Accauntant.AskForBonus(worker, 303))
+                Console.WriteLine("Give me money!!!");
+            else Console.WriteLine("Okkkeeeeey");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Enums/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+
+
+    class Program
+    {
+        static class MyStaticClass
+        {
+            public static void Print(string str, int color)
+            {
+                Console.ForegroundColor = (ConsoleColor)color;
+                Console.WriteLine(str);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of color: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter string: ");
+            string str = Console.ReadLine();
+
+            MyStaticClass.Print(str, n);
+
+
+            Console.ReadLine();
+        }
+    }
+}
